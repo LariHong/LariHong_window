758e9df38dcb8cd317bd1b7980bef0424ef67f39
LariHong <gccbs05591job@gmail.com>
2024-07-02 16:51:09 +0800

    24

---
+++ insert_data/cron.py
@@ -0,0 +1,8 @@
+from crontab import CronTab
+import os
+
+cron=CronTab(user=True)
+path=os.path.abspath("./lesson2.py")
+job=cron.new(command=f"/home/pi/miniconda3/bin/python '{path}'")
+job.minute.every(4)
+cron.write()
